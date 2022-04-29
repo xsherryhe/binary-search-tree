854feb7ff3de9f239e2c3bec1711ebec66a82cd4
Sherry He <xsherryhe@gmail.com>
2022-04-28 21:14:28 -0400

    Completed find and traversal methods

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -17,7 +17,7 @@ class Tree
   attr_accessor :root
 
   def initialize(arr)
-    @root = build_tree(arr.uniq.sort)
+    @root = arr.empty? ? nil : build_tree(arr.uniq.sort)
   end
 
   def build_tree(arr, start = 0, finish = arr.size - 1)
@@ -45,12 +45,11 @@ class Tree
 
   def delete_node(target_node, node)
     return unless node
+    return node.left && node.right ? delete_two_child_node(node) : delete_one_child_node(node) if target_node == node
 
     node.left = delete_node(target_node, node.left) if target_node < node
     node.right = delete_node(target_node, node.right) if target_node > node
-    return node unless target_node == node
-
-    node.left && node.right ? delete_two_child_node(node) : delete_one_child_node(node)
+    node
   end
 
   def delete_one_child_node(node)
@@ -66,6 +65,71 @@ class Tree
 
   def min(node)
     node.left ? min(node.left) : node
+  end
+
+  def find(value, node = root)
+    return unless node
+    return node if value == node.value
+
+    find(value, node.left) if value < node.value
+    find(value, node.right) if value > node.value
+  end
+
+  def level_order_it(&block)
+    return [] unless root
+
+    queue = [root]
+    pointer = 0
+    while pointer < queue.size
+      node = queue[pointer]
+      queue += [node.left, node.right].compact
+      block&.call(node)
+      pointer += 1
+    end
+    queue
+  end
+
+  def level_order_rec(queue = [root], pointer = 0, &block)
+    return [] unless root
+    return queue if pointer >= queue.size
+
+    node = queue[pointer]
+    block&.call(node)
+    level_order_rec(queue + [node.left, node.right].compact, pointer + 1, &block)
+  end
+
+  alias level_order level_order_it
+
+  def in_order(node = root, &block)
+    return [] unless node
+
+    array = []
+    array += in_order(node.left, &block)
+    block&.call(node)
+    array << node
+    array += in_order(node.right, &block)
+    array
+  end
+
+  def pre_order(node = root, &block)
+    return [] unless node
+
+    array = []
+    block&.call(node)
+    array << node
+    array += pre_order(node.left, &block)
+    array += pre_order(node.right, &block)
+    array
+  end
+
+  def post_order(node = root, &block)
+    return [] unless node
+
+    array = []
+    array += post_order(node.left, &block)
+    array += post_order(node.right, &block)
+    block&.call(node)
+    array << node
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
@@ -84,3 +148,10 @@ tree.delete(67)
 tree.pretty_print
 tree.delete(8)
 tree.pretty_print
+tree.delete(20)
+tree.pretty_print
+tree.level_order { |node| p node.value }
+tree.level_order_rec { |node| p node.value }
+tree.in_order { |node| p node.value }
+tree.pre_order { |node| p node.value }
+tree.post_order { |node| p node.value }
